356fe2350c9cdbd0a05083a4c3957729e229ba17
MattSi <mian.si@outlook.com>
2014-04-25 10:15:45 +0800

    add solved problem 100

--- 100.c
+++ 100.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 
-
+#define MAX(a,b) (a>b)?a:b
+#define MIN(a,b) (a<b)?a:b
 int main(int argc, char* argv[]){
 
     int i, j,  p, l, maxlen;
+    int ii, jj;
     long long k;
 #ifndef ONLINE_JUDGE
     freopen("./tests/100.in", "r", stdin);
 #endif
-    while(scanf("%d%d", &i, &j)>0){
-        maxlen = 0;
+    while(scanf("%d%d", &ii, &jj)>0){
+        maxlen = 1;
+        i = MIN(ii,jj);
+        j = MAX(ii,jj);
         for(p=i; p<=j; p++){
             l = 1;
             k = p;
@@ -21,7 +25,7 @@ int main(int argc, char* argv[]){
             if(l>maxlen)
                 maxlen = l;
         }
-        printf("%d %d %d\n", i, j, maxlen);
+        printf("%d %d %d\n", ii, jj, maxlen);
     }
     return 0;
 }
