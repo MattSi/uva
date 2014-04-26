b84562336ed408cbeda029cef795cdfa897b9930
MattSi <mian.si@outlook.com>
2014-04-26 15:56:50 +0800

    Update 706.c
    add resovled problem

--- 706.c
+++ 706.c
@@ -35,12 +35,10 @@ int main(int argc, char* argv[]){
         for(i=0; i<len; i++){
             draw_digit(i, s, num[i]-'0');         
         }
-        printf("%d\n", (s+2)*len +len-1);
         for(i=0; i<2*s+3; i++){
-            for(j=0; i<(s+2)*len+len-1; j++)
+            for(j=0; j<(s+2)*len+len-1; j++)
                 printf("%c", matrix[i][j]);
             printf("\n");
-            printf("=======================\n");
         }
         printf("\n");
     }
@@ -50,40 +48,42 @@ void draw_digit(int col,int s, int digit){
     int rcol = col *(s+3); /* starter of the col */
     int i, j;
     char *p = digits[digit];
-    for(i=0; i<7; i++){
-        /*printf("%c", p[i]);*/
-        switch(i)
-        {
-            case 0:
-            for(j=0; j<s; j++)
-                matrix[0][rcol+1+j] = '-';
-                break;
-            case 1:
-            for(j=0; j<s; j++)
-                matrix[1+j][rcol] = '|';
-                break;
-            case 2:
-            for(j=0; j<s; j++)
-                matrix[1+j][rcol+s+1] = '|';
-                break;
-            case 3:
-            for(j=0; j<s; j++)
-                matrix[s+1][rcol+1+j] = '-';
-                break;
-            case 4:
-            for(j=0; j<s; j++)
-                matrix[1+j+s][rcol] = '|';
-                break;
-            case 5:
-            for(j=0; j<s; j++)
-                matrix[1+j+s][rcol+s+1] = '|';
-                break;
-            case 6:
-            for(j=0; j<s; j++)
-                matrix[2*s+2][rcol+1+j] = '-';
-                break;
-            default:
-                break;
-        }
-    }
+	for(i=0; i<7; i++){
+		if(p[i] == '0');
+		else{
+			switch(i)
+			{
+			case 0:
+				for(j=0; j<s; j++)
+					matrix[0][rcol+1+j] = '-';
+				break;
+			case 1:
+				for(j=0; j<s; j++)
+					matrix[1+j][rcol] = '|';
+				break;
+			case 2:
+				for(j=0; j<s; j++)
+					matrix[1+j][rcol+s+1] = '|';
+				break;
+			case 3:
+				for(j=0; j<s; j++)
+					matrix[s+1][rcol+1+j] = '-';
+				break;
+			case 4:
+				for(j=0; j<s; j++)
+					matrix[1+j+s+1][rcol] = '|';
+				break;
+			case 5:
+				for(j=0; j<s; j++)
+					matrix[1+j+s+1][rcol+s+1] = '|';
+				break;
+			case 6:
+				for(j=0; j<s; j++)
+					matrix[2*s+2][rcol+1+j] = '-';
+				break;
+			default:
+				break;
+			}
+		}
+	}
 }
