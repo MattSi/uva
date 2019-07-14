68550f42ad19facb84ac2771c549d9f587cddb99
Matt Si <mian.si@outlook.com>
2019-07-14 19:18:57 +0800

    update 1598

--- 1598.cpp
+++ 1598.cpp
@@ -44,20 +44,68 @@ struct cmp_more{
     }
 };
 
+int n;
+priority_queue<CMD, vector<CMD>, cmp_less> buy_queue;
+priority_queue<CMD, vector<CMD>, cmp_more> sell_queue;
+map<int, CMD*> id_map;
+map<int, int> buy_cnt;
+map<int, int> sell_cnt;
+
 void find_trade(char trigger){
     if(trigger == 'B'){
-    
     }else{
     }
 }
 
 void print_quote(){
+/*
+    CMD b_order = buy_queue.top();
+    CMD s_order = sell_queue.top();
+    map<int, int>::iterator iter;
+    printf("QUOTE ");
+
+    iter = buy_cnt.find(b_order.price);
+    if(iter != buy_cnt.end()){
+        printf("%d %d", b_order.price, b_order.quant);
+    } else {
+        printf("0 0");
+    }
+    printf(" - ");
+
+    iter = sell_cnt.find(s_order.price);
+    if(iter != sell_cnt.end()){
+        printf("%d %d", s_order.price, s_order.quant);
+    }else {
+        printf("0 99999");
+    }
+    printf("\n");
+*/
 }
 
-int n;
-priority_queue<CMD, vector<CMD>, cmp_less> buy_queue;
-priority_queue<CMD, vector<CMD>, cmp_more> sell_queue;
-map<int, CMD*> id_map;
+void update_cnt(char ins, int price, int quant){
+    map<int, int>::iterator iter;
+    if(ins == 'B'){
+        iter = buy_cnt.find(price);
+        if( iter != buy_cnt.end()){
+            int tmp_q = buy_cnt[price];
+            buy_cnt[price] = quant + tmp_q;
+        }else{
+            buy_cnt[price] = quant;
+        }
+    }else if(ins == 'S'){
+        iter = sell_cnt.find(price);
+        if( iter != sell_cnt.end()){
+            int tmp_q = sell_cnt[price];
+            sell_qnt[price] = quant + tmp_q;
+        } else {
+            sell_cnt[price] = quant;
+        }
+    }else{
+        // CANCEL 取消指令
+
+    }
+}
+
 
 int main(int argc, char **argv){
 #ifndef ONLINE_JUDGE
@@ -67,6 +115,8 @@ int main(int argc, char **argv){
         while(!buy_queue.empty())buy_queue.pop();
         while(!buy_queue.empty())sell_queue.pop();
         id_map.clear();
+        buy_cnt.clear();
+        sell_cnt.clear();
 
         char inst[8];
         int quant, price;
@@ -77,33 +127,37 @@ int main(int argc, char **argv){
                 switch(inst[0]){
                     case 'b':
                     case 'B':
+                        update_cnt('B', price, quant); 
                         buy_queue.push(*c);
+                        find_trade('B');
                         break;
                     case 's':
                     case 'S':
+                        update_cnt('S', price, quant); 
                         sell_queue.push(*c);
+                        find_trade('S');
                         break;
-                    break:
+                    default:
                         break;
                 }
             }else {
                 CMD* cmd = id_map.at(quant); //quant在这里等于要取消的交易id
                 cmd->valid = false;
+                update_cnt('C', cmd->id, 0);
             }
+            print_quote();
         }
-        printf("======End\n");
     }
+    //CMD *c = new CMD(1,100, 35, true);
+    //buy_queue.push(*c);
+    //id_map.insert(pair<int, CMD*>(1, c));
     /*
-    CMD *c = new CMD(1,100, 35, true);
-    buy_queue.push(*c);
-    id_map.insert(pair<int, CMD*>(1, c));
-    buy_queue.push(new CMD(1, 100, 35, true));
-    buy_queue.push(new CMD(2, 100, 34, true));
-    buy_queue.push(new CMD(3, 100, 32, true));
-    buy_queue.push(new CMD(4, 100, 36, true));
-    buy_queue.push(new CMD(5, 100, 35, true));
-*/
-    /*
+    buy_queue.push(*new CMD(1, 100, 35, true));
+    buy_queue.push(*new CMD(2, 100, 34, true));
+    buy_queue.push(*new CMD(3, 100, 32, true));
+    buy_queue.push(*new CMD(4, 100, 36, true));
+    buy_queue.push(*new CMD(5, 100, 35, true));
+
     while (!buy_queue.empty()){
         printf("ID: %5d, PRICE: %5d\n", buy_queue.top().id, buy_queue.top().price);
         buy_queue.pop();
