4c0087f6890aebfdc29b713342ec8dd4559fbef7
MattSi <mian.si@outlook.com>
2019-07-20 07:58:24 +0800

    227

--- 227.cpp
+++ 227.cpp
@@ -12,11 +12,36 @@
 #include <cstring>
 #include <cctype>
 
+const int SIZE = 5;
+char puzzle[SIZE][SIZE+5];
+
+bool read_puzzle(int &x, int &y){
+	memset(puzzle, 0, sizeof(puzzle));
+	char ch;
+	for(int i=0; i<SIZE; i++){
+		fgets(puzzle[i], SIZE+5, stdin);
+		puzzle[i][SIZE]='\0';
+		if(puzzle[i][0] == 'Z'){
+			return false;
+		}
+		for(int j=0; j<SIZE; j++){
+			if(puzzle[i][j] == ' '){
+				x = i;
+				y = j;
+			}
+		}
+	}
+
+	return true;
+}
+
 int main() {
 #ifndef ONLINE_JUDGE
 	FILE *fp = freopen("./tests/227.in", "r", stdin);
 #endif
-
-	
+	int x, y;
+	while(read_puzzle(x, y)){
+	//PUZZLE输入合法，接下来读入指令。	
+	}
 	return 0;
 }
