0f0b6d86709f1dc373ed83a5659d6357c416358a
MattSi <mian.si@outlook.com>
2019-07-23 15:12:32 +0800

    679

--- 679.cpp
+++ 679.cpp
@@ -1,129 +1,56 @@
 /*
 UVa:679
+有棵二叉树，最大深度为D，且所有叶子的深度都相同。所有节点从上到下，从左到右编号为1，2，3，。。。，2^D-1，在节点1出放入一个小球，它会往下落。
+每个内节点上都有一个开关，初始全部关闭，每次小球落在开关上，状态都会改变。当小球到达一个内节点时，如果该节点上的开关关闭，则往左，否则往右。
+直到走到叶子节点。
+一些小球从节点1出开始下落，最后一个小球将会落在哪里呢？
+输入叶子深度D和小球个数I。
+
+输出第I个小球最后所在的叶子编号。假设I不超过整棵树的叶子个数。D<=20, 输入最多包含1000组数据。
+样例输入：
+4 2
+3 4
+10 1
+2 2
+8 128
+16 12345
+
+样例输出：
+12
+7
+512
+3
+255
+36358
 
 */
 
 
 #include <cstdio>
-#include <iostream>
 #include <cstdlib>
 #include <cstring>
-#include <cctype>
+using namespace std;
 
-const int SIZE = 5;
-const int BUFF_SIZE = 8192;
-char puzzle[SIZE][SIZE+5];
-char cmds[BUFF_SIZE];
+const int maxd = 20;
+int s[1<<maxd];
 
-bool read_puzzle(int &x, int &y){
-	memset(puzzle, 0, sizeof(puzzle));
-	for(int i=0; i<SIZE; i++){
-		fgets(puzzle[i], SIZE+5, stdin);
-		puzzle[i][SIZE]='\0';
-		if(puzzle[i][0] == 'Z'){
-			return false;
-		}
-		for(int j=0; j<SIZE; j++){
-			if(puzzle[i][j] == ' '){
-				x = i;
-				y = j;
-			}
-		}
-	}
-
-	return true;
-}
-
-bool boundary_check(int r, int c){
-	if(r<0 || r>=SIZE || c<0||c>=SIZE){
-		return false;
-	}
-	return true;
-}
-
-bool move_puzzle(int &r, int &c){
-	size_t len = strlen(cmds);
-	for(int i=0; i<len; i++){
-		switch(cmds[i]){
-		case 'A':
-			if(!boundary_check(r-1, c))
-				return false;
-			else {
-				puzzle[r][c] = puzzle[r-1][c];
-				puzzle[r-1][c] = ' ';
-				r--;
-			}
-			break;
-		case 'B':
-			if(!boundary_check(r+1, c))
-				return false;
-			else {
-				puzzle[r][c] = puzzle[r+1][c];
-				puzzle[r+1][c] = ' ';
-				r++;
-			}
-			break;
-		case 'R':
-			if(!boundary_check(r, c+1))
-				return false;
-			else {
-				puzzle[r][c] = puzzle[r][c+1];
-				puzzle[r][c+1] = ' ';
-				c++;
-			}
-			break;
-		case 'L':
-			if(!boundary_check(r, c-1))
-				return false;
-			else {
-				puzzle[r][c] = puzzle[r][c-1];
-				puzzle[r][c-1] = ' ';
-				c--;
-			}
-			break;
-		case '0':
-			return true;
-			break;
-		case '\r':
-		case '\n':
-			break;
-		default:
-			return false;
-		}
-	}
-	return true;
-}
 
 int main() {
 #ifndef ONLINE_JUDGE
-	FILE *fp = freopen("./tests/227.in", "r", stdin);
+	FILE *fp = freopen("./tests/679.in", "r", stdin);
 #endif
-	int r, c;
-	int ch, i, cases=0;
-
-	while(read_puzzle(r, c)){
-		//PUZZLE输入合法，接下来读入指令。	
-		memset(cmds, 0, sizeof(cmds));
-		i =0;
-		while(true){
-			ch = getchar();
-			cmds[i++] = ch;
-			if(ch == '0'){
-				getchar();
-				break;
-			}
+	int cases, D, I;
+	scanf("%d", &cases);
+	while(cases--){
+		scanf("%d%d", &D, &I);
+		int k = 1;
+		for(int i=0; i<D-1; i++){
+			if(I%2){k *=2; I = (I+1)/2;}
+			else{k =k*2 +1; I/=2;}
 		}
-		if(cases >0) printf("\n");
-		printf("Puzzle #%d:\n", ++cases);
-		if(move_puzzle(r, c)){
-			//Print Puzzle
-			for(i=0; i<SIZE; i++){
-				printf("%c %c %c %c %c\n", puzzle[i][0], puzzle[i][1],  puzzle[i][2], puzzle[i][3], puzzle[i][4]);
-			}
-		} else {
-			// Print Error
-			printf("This puzzle has no final configuration.\n");
-		}
+		printf("%d\n", k);
 	}
+
+	
 	return 0;
 }
