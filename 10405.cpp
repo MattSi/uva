27ef5f4e1820b4469b4b86122502d80cb029b6c5
MattSi <mian.si@outlook.com>
2019-08-14 07:03:55 +0800

    10405

---
+++ 10405.cpp
@@ -0,0 +1,36 @@
+/*
+UVa:10405 
+求两个字符串的最大公共子串
+*/
+
+
+#include <cstdio>
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+#ifndef ONLINE_JUDGE
+	FILE* fp = freopen("./tests/10405.in", "r", stdin);
+#endif
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
