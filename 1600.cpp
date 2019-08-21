63428247ea7b85dc3096b79f570c19dcc54c3dca
MattSi <mian.si@outlook.com>
2019-08-21 15:18:41 +0800

    1600

--- 1600.cpp
+++ 1600.cpp
@@ -1,80 +1,49 @@
-/*
-UVa:1600
-巡逻机器人， 一个机器人要从一个m*n的网格，左上角走到右下角。空地用0表示， 障碍用1表示。
-机器人每次可以往4个方向走1格，但不能连续穿越k个障碍。(0<=k<=20) 求最短长度。
-起点和终点保证是空地。
-
-*/
-
-
-#include <cstdio>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
+#include <stdio.h> 
+#include <string.h>
 #include <queue>
-#include <algorithm>
-
 using namespace std;
 
-const int SIZE = 25;
-int board[SIZE][SIZE];
-int   vis[SIZE][SIZE];
-const int R[] = { 0, -1, 0, 1 };
-const int C[] = { -1, 0, 1, 0 };
-
-struct Node {
-	int r;
-	int c;
-	int step;
-	Node(int rr, int cc, int sstep) :r(rr), c(cc), step(sstep) {}
-};
-queue<Node> que;
-int K;
-
-bool valid_position(int r, int c) {
-	if(r>=1 &&r<=)
-}
-void push_queue(Node& node) {
-	for (int i = 0; i < 4; i++) {
-
-	}
-}
-
-int travel(int m, int n) {
-	int step = 0;
-	que.push(Node(1,1,step));
-	vis[1][1] = step;
-
-	while (!que.empty()) {
-		Node tmp = que.front(); que.pop();
-		int r = tmp.r;
-		int c = tmp.c;
-		int s = tmp.step;
-		if (r == m && c == n) {
-			return s;
-		}
-		push_queue(tmp);
-	}
-	return -1;
-}
+int g[32][32];
+int dist[32][32][32];
+const int dx[] = {0, 0, 1, -1};
+const int dy[] = {1, -1, 0, 0};
 int main() {
-#ifndef ONLINE_JUDGE
-	FILE* fp = freopen("./tests/1600.in", "r", stdin);
-#endif
-	int cases, m, n;
-	scanf("%d", &cases);
-	while (cases--) {
-		scanf("%d%d", &m, &n);
-		scanf("%d", &K);
-		memset(board, 0, sizeof(board));
-		memset(vis, -1, sizeof(vis));
-		for (int i = 1; i <= m; i++)
-			for (int j = 1; j <= n; j++)
-				scanf("%d", &board[i][j]);
-		while (!que.empty())que.pop();
-		int ans = travel(m, n);
-		printf("%d\n", ans);
+	int testcase;
+	int N, M, K;
+	scanf("%d", &testcase);
+	while (testcase--) {
+		scanf("%d %d %d", &N, &M, &K);
+		for (int i = 0; i < N; i++)
+			for (int j = 0; j < M; j++)
+				scanf("%d", &g[i][j]);
+		memset(dist, 63, sizeof(dist));
+		dist[0][0][0] = 0;
+		queue<int> X, Y, S;
+		int x, y, s, tx, ty, ts;
+		X.push(0), Y.push(0), S.push(0);
+		while (!X.empty()) {
+			x = X.front(), X.pop();
+			y = Y.front(), Y.pop();
+			s = S.front(), S.pop();
+			for (int i = 0; i < 4; i++) {
+				tx = x + dx[i], ty = y + dy[i];
+				if (tx < 0 || ty < 0 || tx >= N || ty >= M)
+					continue;
+				if (g[tx][ty])
+					ts = s + 1;
+				else
+					ts = 0;
+				if (ts > K)	continue;
+				if (dist[tx][ty][ts] > dist[x][y][s] + 1) {
+					dist[tx][ty][ts] = dist[x][y][s] + 1;
+					X.push(tx), Y.push(ty), S.push(ts);
+				}
+			}
+		}
+		int ret = 0x3f3f3f3f;
+		for (int i = 0; i <= K; i++)
+			ret = min(ret, dist[N-1][M-1][i]);
+		printf("%d\n", ret == 0x3f3f3f3f ? -1 : ret);
 	}
-
 	return 0;
 }
